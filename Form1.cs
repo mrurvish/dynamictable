f1d93acab857e2198c889d55f771047d9af7f476
mrurvish <104673044+mrurvish@users.noreply.github.com>
2023-01-16 09:20:32 +0530

    Add files via upload
    connected

--- Form1.cs
+++ Form1.cs
@@ -10,14 +10,13 @@ using System.Data;
 using System.Data.Sql;
 using System.Data.SqlClient;
 
-
-namespace dynamicdata
+namespace database
 {
     public partial class Form1 : Form
     {
-        int textbox = 0;
-        int datatype = 0;
-       
+        int id = 0;
+        int rc = 0;
+        int lastid = 0;
         public Form1()
         {
             InitializeComponent();
@@ -25,96 +24,146 @@ namespace dynamicdata
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (tbname == null&&nofilds== null)
+            Class1.cn.Open();
+            string sql = "insert into Table2 values('" + textBox1.Text + "','" + textBox2.Text + "')";
+            SqlCommand cmd = new SqlCommand(sql, Class1.cn);
+            cmd.ExecuteNonQuery();
+            Class1.cn.Close();
+            loaddata();
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           /* Class1.cn.Open();
+            id += 1;
+            
+            
+                string sql1 = "select * from table2 where id='" + id + "'";
+                SqlCommand cmd1 = new SqlCommand(sql1, Class1.cn);
+                SqlDataReader dr1 = null;
+                dr1 = cmd1.ExecuteReader();
+                string idcol1 = null;
+                while (dr1.Read())
+                {
+                    textBox1.Text = dr1[1].ToString();
+                    textBox2.Text = dr1[2].ToString();
+                     idcol1 = dr1[0].ToString();
+                }
+                Class1.cn.Close();*/
+            if (id != lastid)
             {
+            lable:
+
+                Class1.cn.Open();
+                id += 1;
+                string sql2 = "select * from table2 where id='" + id + "'";
+                SqlCommand cmd2 = new SqlCommand(sql2, Class1.cn);
+                SqlDataReader dr2 = null;
+                dr2 = cmd2.ExecuteReader();
+                string ids = null;
+                while (dr2.Read())
+                {
+                    ids = dr2[0].ToString();
+                    textBox1.Text = dr2[1].ToString();
+                    textBox2.Text = dr2[2].ToString();
+
+                }
+                if (ids == null)
+                {
+                    Class1.cn.Close();
+                    goto lable;
+                }
+                else
+                {
+                    Class1.cn.Close();
+
+                }
+                Class1.cn.Close();
 
             }
-            else
-            {
-                int fild = Convert.ToInt32(nofilds.Text);
 
-                for (int i = 0; i < fild; i++)
+           
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (id != 0 && id!=1)
+            {
+            lable:
+
+                Class1.cn.Open();
+                id -= 1;
+                string sql2 = "select * from table2 where id='" + id + "'";
+                SqlCommand cmd2 = new SqlCommand(sql2, Class1.cn);
+                SqlDataReader dr2 = null;
+                dr2 = cmd2.ExecuteReader();
+                string ids = null;
+                while (dr2.Read())
                 {
-                    TextBox txt = new TextBox();
-                    this.Controls.Add(txt);
-                    txt.Left = textbox * 60;
-                    txt.Name = "fildname" + i;
-                    txt.Top = 130;
-                    //txt.Text = "TextBox" + this.textbox.ToString();
-                    //  textbox = textbox * 60;
-                  
-                    textbox = textbox + 2;
+                    ids = dr2[0].ToString();
+                    textBox1.Text = dr2[1].ToString();
+                    textBox2.Text = dr2[2].ToString();
+
                 }
-                for (int i = 0; i < fild; i++)
+                if (ids == null)
                 {
-                    TextBox txt = new TextBox();
-                    this.Controls.Add(txt);
-                    txt.Left = datatype * 60;
-                    txt.Name = "dtype" + i;
-                    txt.Top = 150;
-                    //txt.Text = "TextBox" + this.textbox.ToString();
-                    //  textbox = textbox * 60;
-                    datatype = datatype + 2;
+                    Class1.cn.Close();
+                    goto lable;
                 }
-                button1.Hide();
-                ctable.Show();
+                else
+                {
+                    Class1.cn.Close();
+
+                }
+                Class1.cn.Close();
 
             }
+        }
+
+
+        private void rowcount()
+        {
+            Class1.cn.Open();
+            string sql = "select * from Table2";
+            SqlCommand cmd = new SqlCommand(sql, Class1.cn);
+            SqlDataReader dr = null;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+               lastid = Convert.ToInt32(dr[0]);
+            }
+          //  MessageBox.Show(rc.ToString());
+            Class1.cn.Close();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            ctable.Hide();
-           
+            rowcount();
+            loaddata();
+            
         }
-
-        private void ctable_Click(object sender, EventArgs e)
+        private void loaddata()
         {
-            try
-            {
-               
+            Class1.cn.Open();
+            string sql = "select * from Table2";
+            SqlCommand cmd = new SqlCommand(sql, Class1.cn);
+            SqlDataReader dr = null;
+            dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            dataGridView1.DataSource = dt;
+            /*  while (dr.Read())
+              {
 
-
-
-
-                Class1.cn.Open();
-                int fild = Convert.ToInt32(nofilds.Text);
-                string sql = "CREATE TABLE " + tbname.Text;
-                sql += " " + "(id integer";
-                for (int i = 0; i < fild; i++)
-                {
-                    TextBox txt = (TextBox)Controls["fildname" + i];
-                    //fildname[i] = txt.Text;
-                    // MessageBox.Show(txt.Text);
-                    TextBox txt1 = (TextBox)Controls["dtype" + i];
-                    //datatye[i] = txt1.Text;
-                    sql += "," + txt.Text + " " + txt1.Text;
-                }
-
-                sql += ")";
-                // MessageBox.Show(sql);
-                SqlCommand cmd = new SqlCommand(sql, Class1.cn);
-                cmd.ExecuteNonQuery();
-                Class1.cn.Close();
-
-               
-                tbname.Text = nofilds.Text = string.Empty;
-                button1.Show();
-                ctable.Hide();
-                for (int ix = 0; ix < fild; ix++)
-                {
-                     this.Controls["fildname" + ix].Dispose();
-                     this.Controls["dtype" + ix].Dispose(); 
-
-                }
-                textbox = 0;
-                datatype = 0;
-                
-                }
-                catch (SqlException E) {
-                    MessageBox.Show("filds is empty or incorrect");
-                    Class1.cn.Close();
-                }
+                  label1.Text = "name="+dr[1].ToString()+"  "+"pwd="+dr[2].ToString();
+                  
+              }*/
+            Class1.cn.Close();
+           rc = dataGridView1.Rows.Count;
+            //MessageBox.Show(row1.ToString());
         }
+
+       
+
     }
 }
